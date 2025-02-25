32e48856b11e108696b84a3221d2bc7e55039dc3
Tofey <105518597+tofeyy@users.noreply.github.com>
2025-02-25 23:20:41 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,42 @@
+{
+    "remote_link": "null",
+    "ads_int_proxy": "false",
+    "delay_appopen": "480",
+    "inters_count": "10",
+
+    "ads_free": "",
+
+    "admob_id": "ca-app-pub",
+    "admob_app_open": "ca-app-pub",
+    "admob_interstitial": "ca-app-pub",
+    "admob_native": "ca-app-pub",
+
+    "ads_native_main": "true",
+    "ads_native_chat": "false",
+    "ads_native_profile": "false",
+
+    "ads_full_proxies": "0",
+    "ads_full_changes": "0",
+    "ads_full_profile": "10",
+
+    "delay_app_open": "8",
+    "delay_native_req": "180",
+    "delay_native_seen": "180",
+    "delay_full": "8",
+    "full_instead_native": "false",
+
+    "blacklist": "",
+    "force_join": "false",
+    "invisible": "true",
+    "show_deleted": "true",
+    "url": "https://mtrb0.website",
+
+    "xmod": "true",
+    "xmod_c": "1000",
+    "test_number1": "5148",
+    "test_number2": "9637",
+
+    "mini_alert": "true",
+    "mini_text": "قم بتسجيل الدخول للقناة للحصول على اخر التحديثات",
+    "mini_link": "https://t.me/B_G_GG"
+}
